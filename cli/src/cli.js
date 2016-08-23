8abc709c49109f861817932505ecb7a490571385
dakarl <dakarl@asu.edu>
2016-08-23 08:11:28 -0500

    Tuesday 8am commit

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -8,9 +8,10 @@ export const cli = vorpal()
 let username
 let server
 let defaultCommand = ''
+let lastPMd = ''  // the last person private messaged
 
 cli
-  .delimiter(cli.chalk['yellow']('ftd~$'))
+  .delimiter(cli.chalk['yellow']('$>'))  //what is shown before you type
 
 cli
   .mode('connect <username> [server] = localhost')
@@ -23,28 +24,30 @@ cli
     })
 
     server.on('data', (buffer) => {
-      this.log(Message.fromJSON(buffer).toString())
-    })
+      if(Message.command === 'echo'){
+      this.log(cli.chalk.cyan(Message.fromJSON(buffer).toString()))
+    } else if(Message.command === 'broadcast'){
+    this.log(cli.chalk.red(Message.fromJSON(buffer).toString()))
+  } else if(Message.command === '@'){
+  this.log(cli.chalk.blue(Message.fromJSON(buffer).toString()))
+} else {
+this.log(cli.chalk.magenta(Message.fromJSON(buffer).toString()))
+}
+  })
 
     server.on('end', () => {
       cli.exec('exit')
     })
   })
   .action(function (input, callback) {
-    let entered = words(input, /\S*\s*/)
+    let entered = words(input, /\S*/g)
     let [ command, ...rest ] = entered
-    const contents = rest.join(' ')
-
-    console.log(entered)
-    console.log(command.charAt(0))
-
-    console.log("start of statement default command : " + defaultCommand)
-
-    console.log("AT 0 :   " + command.charAt(0))
+    let contents = rest.join(' ')
 
     if (command === 'disconnect') {
       server.end(new Message({ username, command }).toJSON() + '\n')
-    } else if (command === 'echo') {
+    }
+    else if (command === 'echo') {
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
       console.log(JSON.stringify({ username, command, contents }))
 
@@ -56,11 +59,13 @@ cli
       command = command.charAt(0)
 
 
-
       const [ ...rest ] = entered
-      const contents = rest.join(' ')
+      contents = rest.join(' ')
 
-      console.log("content: " + contents)
+
+      lastPMd = contents.substr(0, contents.indexOf(" "));
+
+      console.log("--------------------- content: " + contents)
 
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
 
@@ -69,7 +74,11 @@ cli
     } else {
       command = defaultCommand
       const [ ...rest ] = entered
-      const contents = rest.join(' ')
+      let contents = rest.join(' ')
+      if (command === '@')
+      {
+        contents = lastPMd + ' ' + contents
+      }
       console.log('username: ' + username + ' command: ' + defaultCommand + ' contents: ' + contents)
 
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -37,19 +37,36 @@ public class ClientHandler implements Runnable {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-			String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+			String timeStamp = new SimpleDateFormat("EEE, MMM d, yyyy, hh:mm:ss aa").format(new Date());
 			
 			while (!socket.isClosed()) {
 				String raw = reader.readLine();
 				Message message = mapper.readValue(raw, Message.class);
 
-				log.info("user <{}> doing command: <{}>", message.getUsername(), message.getCommand());
+				timeStamp = new SimpleDateFormat("EEE, MMM d, yyyy, hh:mm:ss aa").format(new Date());
+				
+				log.info("user <{}> doing command: <{}>  :: {}", message.getUsername(), message.getCommand(), timeStamp);
 				
 				switch (message.getCommand()) {
 					case "connect":
 						log.info("user <{}> connected", message.getUsername());
 						users.add(new User(message.getUsername(), this.socket));
 						
+						message.setContents(timeStamp + " <" + message.getUsername() + "> has connected.");
+						
+						String cMsg = mapper.writeValueAsString(message);
+						
+						for(int i = 0; i < users.size(); ++i)
+						{
+							writer = new PrintWriter(new OutputStreamWriter(users.get(i).getSocket().getOutputStream()));
+							writer.write(cMsg);
+							writer.flush();
+						}						
+						
+						
+						
+						
+						//debugging purposes only
 						log.info("Users found: ");
 
 						for(int i = 0; i < users.size(); ++i)
@@ -61,10 +78,20 @@ public class ClientHandler implements Runnable {
 						break;
 					case "disconnect":
 						log.info("user <{}> disconnected", message.getUsername());
-						message.setContents(message.getUsername() + " has disconnected.");
+						
+						message.setContents(timeStamp + " <" + message.getUsername() + "> has disconnected.");
+						
 						String dcMsg = mapper.writeValueAsString(message);
-						writer.write(dcMsg);
-						writer.flush();
+						
+						for(int i = 0; i < users.size(); ++i)
+						{
+							writer = new PrintWriter(new OutputStreamWriter(users.get(i).getSocket().getOutputStream()));
+							writer.write(dcMsg);
+							writer.flush();
+						}
+						
+						
+						
 						this.socket.close();
 						
 						users.remove(message.getUsername());
@@ -73,6 +100,8 @@ public class ClientHandler implements Runnable {
 						break;
 					case "echo":
 						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
+						String echoBuffer = message.getContents();
+						message.setContents(timeStamp + " <" + message.getUsername() + "> (echo): " + echoBuffer);
 						String response = mapper.writeValueAsString(message);
 						writer.write(response);
 						writer.flush();
@@ -101,14 +130,15 @@ public class ClientHandler implements Runnable {
 						
 						boolean userFound = false;
 						
-						log.info("DEBUGGING @ INFO: " + message.getContents() + " ---- " + message.getContents().substring(message.getContents().indexOf(' ')));
+						log.info("DEBUGGING @ INFO: full msg:" + message.getContents() + " ---- user being sent to: " + toUser + "  actual msg: " + message.getContents().substring(message.getContents().indexOf(' ')));
 						
 						msgBuffer = message.getContents().substring(message.getContents().indexOf(' '));
 						message.setContents(timeStamp + " <" + message.getUsername() + "> (whisper): " + msgBuffer);
 
 						for(int i = 0; i < users.size(); ++i)
 						{
-							if(users.get(i).getUsername() == toUser)
+							log.info("current user options, one should match the user being msg'd :  " + users.get(i).getUsername());
+							if(users.get(i).getUsername().equals(toUser))
 							{
 								userFound = true;
 								writer = new PrintWriter(new OutputStreamWriter(users.get(i).getSocket().getOutputStream()));
